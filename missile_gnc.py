cc8ee52e4f31745469f08e276f57ce73cf6bc7ac
khushant2001 <khushant.khurana@cooper.edu>
2023-12-04 10:08:23 -0500

    Update to the model and the code

--- missile_gnc.py
+++ missile_gnc.py
@@ -19,13 +19,22 @@ D = np.array([
     [0]
 ])
 
+# Defining Q and R matrices to select weights on the states and input.
 Q = np.array([
     [1,0],
-    [0,10]
+    [0,.1]
 ])
 R = np.array([
-    [1]
+    [.01]
 ])
 sys = ss(A,B,C,D)
 k,_,_ = lqr(sys,Q,R)
-print(k)
+
+print("Eigenvalues of the system are = ", np.linalg.eig(A)[0])
+print("LQR gain for the system = ",k)
+
+# Finding the kalman gain 
+# The error between actual state and estimated state is defined as e_dot = [A - C*k]*e
+pole_position = [-50,-100]
+ke = place(A,C,pole_position)
+print("Kalman gain for the system = ", ke)
